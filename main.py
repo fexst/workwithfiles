e83691186855feb343ef68d4c6c02ede70a812eb
Konstantin <shuklin.kostya@mail.ru>
2024-11-22 20:07:28 +0300

    add function

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-with open('1.txt', encoding='UTF-8') as f1:
-    data1 = f1.readlines()
-    count_lines1 = len(data1)
-    files_data1 = (f1.name, count_lines1, data1)
-    with open('2.txt', encoding='UTF-8') as f2:
-        data2 = f2.readlines()
-        count_lines2 = len(data2)
-        files_data2 = (f2.name, count_lines2, data2)
-        with open('3.txt', encoding='UTF-8') as f3:
-            data3 = f3.readlines()
-            count_lines3 = len(data3)
-            files_data3 = (f3.name, count_lines3, data3)
-            file_data_all = [files_data1, files_data2, files_data3]
-            sorted_files = sorted(file_data_all, key=lambda x: x[1])
-            with open('result.txt', 'w', encoding='utf-8') as result_file:
+import os
+
+def initial():
+    path = os.getcwd()
+    file_data_all = []
+    for file in os.listdir(path):
+        if file.endswith('.txt') and file[0].isdigit():
+            with open(file, encoding='UTF-8') as f:
+                data = f.readlines()
+                count_lines = len(data)
+                files_data = (f.name, count_lines, data)
+                file_data_all.append(files_data)
+    sorted_files = sorted(file_data_all, key=lambda x: x[1])
+    with open('result.txt', 'w', encoding='utf-8') as result_file:
                 for file_name, count_lines, all_text in sorted_files:
                     result_file.write(f'{file_name}\n')
                     result_file.write(f'{count_lines}\n')
@@ -23,6 +22,4 @@ with open('1.txt', encoding='UTF-8') as f1:
                             result_file.write(f'{text}\n')
 
 
-
-                
-
+initial()
